106b427f554c3067a17e89c30fb0d4e9c584a1fb
acongpham <39377276+acongpham@users.noreply.github.com>
2019-10-22 16:47:58 +0700

    Khoidau
    themToolba và Navigation bar

--- PandaApp/app/src/main/java/com/example/pandaapp/MainActivity.java
+++ PandaApp/app/src/main/java/com/example/pandaapp/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.pandaapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+    Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        toolbar.setNavigationIcon(R.drawable.back1);
+        getSupportActionBar().ico
     }
 }
