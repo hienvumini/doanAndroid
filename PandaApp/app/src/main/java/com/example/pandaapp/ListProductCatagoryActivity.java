67839f4fb6e914682bc6837f249ba24421062239
hieustar <56863942+hieustar@users.noreply.github.com>
2019-10-23 16:41:22 +0700

    push lan 2

---
+++ PandaApp/app/src/main/java/com/example/pandaapp/ListProductCatagoryActivity.java
@@ -0,0 +1,14 @@
+package com.example.pandaapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListProductCatagoryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_product_catagory);
+    }
+}
